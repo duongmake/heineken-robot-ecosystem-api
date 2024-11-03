c1a9d5b940a2a0246263d25790f421387fb5a800
duongmake <duongmake3@gmail.com>
2024-11-03 23:44:21 +0700

    commit Create Robot API

---
+++ RobotAPI/Entities/Robot.cs
@@ -0,0 +1,12 @@
+﻿namespace RobotAPI.Entities
+{
+    public class Robot
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public DateTime LastAccessTime { get; set; }
+        public int BatteryLevel { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ RobotAPI/FluentAPI/RobotConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RobotAPI.Entities;
+using System.Security.Principal;
+
+namespace RobotAPI.FluentAPI
+{
+    public class RobotConfiguration : IEntityTypeConfiguration<Robot>
+    {
+        public void Configure(EntityTypeBuilder<Robot> builder)
+        {
+            builder.ToTable("Robot");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Status).IsRequired();
+            builder.Property(x => x.LastAccessTime).IsRequired();
+            builder.Property(x => x.BatteryLevel).IsRequired();
+            builder.Property(x => x.CreatedDate).IsRequired();
+        }
+    }
+}
